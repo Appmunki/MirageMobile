2f079565fa8ce40501a7da1765d56232742d1548
DiegoAmezquita <amdgerman@gmail.com>
2013-08-19 22:34:29 -0500

    new environment variable for opencv path

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 #opencv
 OPENCV_LIB_TYPE:=STATIC
 OPENCV_INSTALL_MODULES:=on
-include /home/diego/OpenCV-2.4.4-android-sdk/sdk/native/jni/OpenCV.mk
+include $(OPENCV)/OpenCV.mk
 
 #ifdef DEBUG
 #CONFIG_DIR  := Debug
