2ba466660bd4fa0d03890227a2c57b6fb215a2e4
Radzell <radzell@appmunki.com>
2014-02-10 03:45:42 -0500

    finish faster version

---
+++ MirageMobileApp/src/com/appmunki/miragemobile/MainARActivity.java
@@ -0,0 +1,28 @@
+package com.appmunki.miragemobile;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import com.appmunki.miragemobile.ar.ARActivity;
+
+public class MainARActivity extends ARActivity {
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test_ar);
+		//initLayout();
+		setupGLSurfaceViewLayout();
+	}
+
+	
+
+	
+
+	
+
+	
+}
